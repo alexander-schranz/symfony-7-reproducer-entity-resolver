2f080fad70b637227f669b82b40b6683931aa36e
Alexander Schranz <alexander@sulu.io>
2024-04-16 21:33:47 +0200

    Add default Symfony 7 debug pack

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,26 @@
 
 namespace App;
 
+use App\Entity\Some;
+use App\Entity\SomeInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+class Kernel extends BaseKernel implements CompilerPassInterface
 {
     use MicroKernelTrait;
+
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->hasDefinition('doctrine.orm.listeners.resolve_target_entity')) {
+            throw new \RuntimeException('Cannot find Doctrine Target Entity Resolver Listener.');
+        }
+
+        $resolveTargetEntityListener = $container->findDefinition('doctrine.orm.listeners.resolve_target_entity');
+
+        $resolveTargetEntityListener
+            ->addMethodCall('addResolveTargetEntity', [SomeInterface::class, Some::class, []]);
+    }
 }
